0a2435ab0b2b01dae9cc51235287eef2e3e3c690
Edinei Pinho dos Santos <edneyrock@gmail.com>
2023-04-11 19:46:27 -0300

    :sparkles: feat(custom menu)

--- functions.php
+++ functions.php
@@ -39,35 +39,7 @@ function remove_some_body_class($classes) {
 add_filter('body_class', 'remove_some_body_class');
 
 
-function format_products($products, $img_size = 'medium')
-{
-  $products_final = [];
-  foreach ($products as $product) {
-    $products_final[] = [
-      'name' => $product->get_name(),
-      'preco' => $product->get_price_html(),
-      'link' => $product->get_permalink(),
-      'img' => wp_get_attachment_image_src($product->get_image_id(), $img_size)[0],
-    ];
-  }
-  return $products_final;
-}
-
-function hendal_product_list($products){ ?>
-<ul class="products-list">
-  <?php foreach ($products as $product) { ?>
-  <li class="product-item">
-    <a href="<?= $product['link'];?>">
-      <div class="product-info">
-        <img src="<?= $product['img']; ?>" alt="<?= $product['name']; ?>" />
-        <h2><?= $product['name']; ?> - <span><?= $product['preco']; ?></span></h2>
-      </div>
-      <div class="product-overlay">
-        <span class="btn-link">Ver Mais</span>
-      </div>
-    </a>
-  </li>
-  <?php } ?>
-</ul>
-<?php } ?>
+  include(get_template_directory() . './inc/user-custom-menu.php');
+  include(get_template_directory() . './inc/product-list-php');
+?>
 <!-- hendal_product_list -->
---
+++ inc/user-custom-menu.php
@@ -0,0 +1,27 @@
+<?php 
+
+// Adicionar novo menu
+function hendal_custom_menu($menu_links){
+  $menu_links = array_slice($menu_links, 0, 5, true)
+  + ['certificados' => 'Certificados']
+  + array_slice($menu_links, 5, NULL, true);
+
+  unset($menu_links['downloads']);
+  
+  return $menu_links;
+}
+add_filter( 'woocommerce_acctount_menu_items', 'hendal_custom_menu' );
+
+function hendal_add_endpoint(){
+  add_rewrite_endpoint('certificados', EP_PAGES);
+}
+add_action('init', 'hendal_add_endpoint');
+
+function hendal_certificados() {
+  echo "<p>Esses são so seus certificados</p>";
+}
+
+add_action('woocommerce_account_certificados_endpoint', 'hendal_certificados');
+
+
+?>
